42a9843a97ed1a37733704dd00cac647859c9e6c
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-10-16 15:35:36 +0000

    Setup template inheritance for tasks.html + base header main and footer

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sidebar initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
