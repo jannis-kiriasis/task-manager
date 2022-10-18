1aaa3c8d64d61bf08ef9d0fe18d3e9d8ca314432
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-10-18 16:29:04 +0000

    Query categories and jinja for loop

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,11 +10,13 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    # if POST save category_name in database
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
